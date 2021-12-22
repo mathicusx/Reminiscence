dee97313ea32cae7744e9eee20a9f38f0f8013e4
Ismail Bekirov <67590354+mathicusx@users.noreply.github.com>
2021-12-22 08:39:06 +0000

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = ()  =>{   // App = () = > { -- put stuff in here--} is a functional component
+    return (
+        <div>
+            <h1>App</h1>
+        </div>
+    )
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import App from './App';
+
+// We Render App, and with the second command we are connecting with the Id of Root
+ReactDom.render(<App />, document.getElementById('root')) 
